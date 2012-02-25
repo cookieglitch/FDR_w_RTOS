c9797159f5f143df7bb87267cb60f3f68b345416
John Tiernan <tiernan.john.j@gmail.com>
2012-02-25 18:13:38 +0000

    Added basic structure, RTOS needed

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
+/**
+ * FDR etc etc
+ * Copyright: John Tiernan 2012
+ */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-void main()
+int init_hardware();
+
+int main()
+{
+	//Hardware setup
+	int setup = init_hardware();
+
+	//Startup tasks
+
+
+	//Main loop
+	while(1)
+	{
+
+
+
+	}
+
+}
+
+int init_hardware(void)
 {
 
-
+	return 0;
 }
